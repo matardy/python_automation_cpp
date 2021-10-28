e991d14b9e5bb7d5424ffec19db68d26a7e34bd5
Gutemberg S. Mendoza <gutembergmendoza@Gutembergs-MacBook-Pro.local>
2021-10-28 14:01:54 -0500

    new colors added, bugs fixed and error controling

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,8 +1,10 @@
 #include<iostream>
+
 using namespace std; 
 
 int main(){
     cout<< "Hello World!"<<endl;
-
+    cout<< "This is a python automatization"<<endl
+    
     return 0; 
 }
--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import os 
+import re
 # This code works better with terminal command key extension in vscode
 
 class Route:
@@ -16,7 +17,7 @@ class Route:
                 break
         return my_file[::-1]
     
-    def delete_cpp_ext(self,cpp_file):
+    def delete_cpp_ext(self,cpp_file)->str:
         my_file_name = ""
         for i in cpp_file:
             if i != ".":
@@ -24,26 +25,61 @@ class Route:
             else:
                 break
         return my_file_name
+    
+    def get_file_type(self, my_file)->bool:
+        return re.search("cpp", my_file)
+#myfile.cpp
 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
 
+    def disable(self):
+        self.HEADER = ''
+        self.OKBLUE = ''
+        self.OKGREEN = ''
+        self.WARNING = ''
+        self.FAIL = ''
+        self.ENDC = ''
 
+def foo():
+    x = input("Here: ")
+    print(x)
 
-
-def run():
-    route_raw = input("press cmd+h ")
+def main():
+    promt = f"{bcolors.HEADER}Press cmd+h -->{bcolors.ENDC}"
+    route_raw = input(promt)
     my_route = Route(route_raw)
     route_cpp = my_route.find_cpp_file()
     route_exe = my_route.delete_cpp_ext(route_cpp)
-    
+    print()
    
-    if os.system("g++ {} -o bin/{}".format(route_cpp,route_exe)) == 0:
-        print("Compiled successfully")
-        print("--------------------------------------------------")
+    if os.system("g++ {} -o bin/{}".format(route_cpp,route_exe)) == 0 :
+        print(f"{bcolors.OKGREEN}----------------------{bcolors.ENDC}")
+        print(f"{bcolors.OKGREEN}Compiled Successfully!{bcolors.ENDC}")
+        print(f"{bcolors.OKGREEN}----------------------{bcolors.ENDC}")
         print('\n')
         os.system("./bin/{}".format(route_exe))
         print('\n')
+    elif re.search("/",route_raw)== None:
+        print(f"{bcolors.FAIL}---------------------------------------------{bcolors.ENDC}")
+        print(f"{bcolors.FAIL}Press cmd+h or check your keyboard shortcuts.{bcolors.ENDC}")
+        print(f"{bcolors.FAIL}---------------------------------------------{bcolors.ENDC}")
+    elif (my_route.get_file_type(route_cpp)) == None:
+        print(f"{bcolors.WARNING}------------------------------------------------------------------------{bcolors.ENDC}")
+        print(f"{bcolors.WARNING}Only works for c++ files. Your active vscode file isn't a c++/.cpp file.{bcolors.ENDC}")
+        print(f"{bcolors.WARNING}------------------------------------------------------------------------{bcolors.ENDC}")
+    
+
     else:
-        print('Debug your code!')
+        print(f"{bcolors.FAIL}----------------{bcolors.ENDC}")
+        print(f"{bcolors.FAIL}Debug your code!{bcolors.ENDC}")
+        print(f"{bcolors.FAIL}----------------{bcolors.ENDC}")
 
 if __name__ == '__main__':
-    run()
+    main()
+    
